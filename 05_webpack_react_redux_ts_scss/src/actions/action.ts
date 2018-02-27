eb5a012b3a28ebe603b9f9c8610d1cee3d6efd28
Stephan Schröder <stephan.schroder@resurs.se>
2018-02-27 15:33:04 +0100

    make sure all ActionType enum values are initialized as strings

--- 05_webpack_react_redux_ts_scss/src/actions/action.ts
+++ 05_webpack_react_redux_ts_scss/src/actions/action.ts
@@ -1,11 +1,15 @@
+// string enums are used instead of nummeric enums because
+// redux-promise-middleware assumes that action.type contains a string
 enum ActionType {
-    INC_A,
-    RESET_A,
+    INC_A = "INC_A",
+    RESET_A = "RESET_A",
     SET_B_PROMISE = "SET_B_PROMISE",
+    // redux-promise-middleware creates actions with these types from the base SET_B_PROMISE
     SET_B_PROMISE_FULFILLED = "SET_B_PROMISE_FULFILLED",
     SET_B_PROMISE_REJECTED = "SET_B_PROMISE_REJECTED",
 }
 
+// redux-promise-middleware checks if action.payload contains a Promise
 type Action = {
     type: ActionType,
     payload?: any
